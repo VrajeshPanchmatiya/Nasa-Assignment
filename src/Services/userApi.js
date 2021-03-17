4f1ca0d36d843bc53db2b1a720bedfdac130f381
vrajesh Panchmatiya <vrajesh.panchmatiya@green-apex.com>
2021-03-17 16:37:00 +0530

    initial commit

--- src/Services/userApi.js
+++ src/Services/userApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const userApi = (id) => {
+  console.log(process.env);
   return axios.get(
     `${process.env.REACT_APP_API_FIXED}${id}?${process.env.REACT_APP_API_KEY}`
   );
